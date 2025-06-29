becef19ffd99a17e0aeabc9b6d21a46d67f5eff9
Thinhpc09328 <nguyenducthinhcn2005@gmail.com>
2025-06-29 14:04:34 +0700

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DATN.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PhanTich()
+        {
+            return View();
+        }
+
+        public IActionResult ChienDich()
+        {
+            return View();
+        }
+        public IActionResult DanhMuc()
+        {
+            return View();
+        }
+        public IActionResult MaGiamGia()
+        {
+            return View();
+        }
+        public IActionResult KhachHang()
+        {
+            return View();
+        }
+        public IActionResult KhoHang()
+        {
+            return View();
+        }
+        public IActionResult DonHang()
+        {
+            return View();
+        }
+        public IActionResult SanPham()
+        {
+            return View();
+        }
+        public IActionResult CaiDat()
+        {
+            return View();
+        }
+    }
+}
