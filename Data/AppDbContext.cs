348598e45f3e144b406393cc554f10f0886d06fd
baott430 <baot76623@gmail.com>
2025-07-02 15:55:36 +0700

    Gom va fix loi danh muc

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -11,6 +11,6 @@ namespace DATN.Data
         }
 
         public DbSet<Product> Products { get; set; }
-        //public DbSet<Category> Categories { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
