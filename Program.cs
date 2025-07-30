5c522d1ca8280d834a726748939c7e2599048b5a
Duyen <duyenntkpc08197@gmail.com>
2025-07-30 16:43:24 +0700

    dgg

--- Program.cs
+++ Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Admin}/{action=MaGiamGia}/{id?}");
+    pattern: "{controller=Category}/{action=Index}/{id?}");
 
 app.Run();
