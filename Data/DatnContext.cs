c174e3d42d8b0b6ef9103b4a787d45f720d7e3c9
miinhkha <miinhkha10@gmail.com>
2025-07-31 11:50:57 +0700

    chx gom het code

--- Data/DatnContext.cs
+++ Data/DatnContext.cs
@@ -43,7 +43,7 @@ namespace DATN.Data;
     public DbSet<ShippingProvider> ShippingProviders { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-C7PM3MB;Initial Catalog=DATN;Integrated Security=True;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-FDN5AM5O\\SQLEXPRESS;Initial Catalog=DATN1;Integrated Security=True;Pooling=False;Encrypt=True;Trust Server Certificate=True;MultipleActiveResultSets=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
